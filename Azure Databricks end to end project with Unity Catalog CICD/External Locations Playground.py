3d3b4d55c4a2855b7c7018623ccf4dd8c91cf974
borystarabarov <borystarabarov>
2024-12-23 18:07:08 +0000

    test

---
+++ Azure Databricks end to end project with Unity Catalog CICD/External Locations Playground.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+df = spark.read.format("parquet") \
+               .load("abfss://external-location@databricksdevstgacc.dfs.core.windows.net/files/")
+
+# COMMAND ----------
+
+display(df)
